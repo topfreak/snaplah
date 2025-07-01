8bdf267e33af0a4e320fe4f057c94f381738724f
Taufiq Hidayatullah <topik.id.x@gmail.com>
2025-07-01 13:05:22 +0700

    snaplah03

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // <-- Impor BrowserRouter
+import { BrowserRouter } from 'react-router-dom';
+import ReactGA from 'react-ga4';
+
 import App from './App.jsx';
 import './index.css';
+
+const MEASUREMENT_ID = "G-NHQMB225WR"; 
+ReactGA.initialize(MEASUREMENT_ID);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
